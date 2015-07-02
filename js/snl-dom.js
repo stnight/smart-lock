3b31e32036545bcd9710ea711256ce86ce547224
John Eric <earljohn3ric@gmail.com>
2015-07-03 06:29:41 +0800

    Created the SNL DOM Lib creator

---
+++ js/snl-dom.js
@@ -0,0 +1,96 @@
+(function() {
+  var __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
+
+  this.DOM = {
+    textNodesArr: ['a', 'p', 'label', 'span', 'li', 'div', 'button', 'textarea', 'option', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
+    registeredDOMarray: [],
+    register: function(toReg) {
+      return Object.keys(toReg).forEach(function(key) {
+        return this.DOM.registeredDOMarray[key] = toReg[key];
+      });
+    },
+    getElem: function(el) {
+      return document.querySelectorAll(el);
+    },
+    makeElem: function(el) {
+      return document.createElement(el);
+    },
+    delElem: function(el) {
+      var element;
+      element = this.DOM.getElem(el);
+      return [].map.call(element, function(el) {
+        return el.parentNode.removeChild(el);
+      });
+    },
+    makeText: function(el) {
+      return document.createTextNode(el);
+    },
+    giveAttributes: function(el, toMake, isTypeOfText) {
+      var attr, eventName, textContent, _i, _j, _len, _len1, _ref, _ref1;
+      if (typeof toMake.attributeList !== 'undefined') {
+        _ref = toMake.attributeList;
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          attr = _ref[_i];
+          el.setAttribute(attr, toMake.attributeList[attr]);
+        }
+      }
+      if (isTypeOfText === true && typeof toMake.textContent !== 'undefined') {
+        textContent = this.DOM.makeText(toMake, textContent);
+        el.appendChild(textContent);
+      }
+      if (typeof tomake.eventFunc !== 'undefined') {
+        _ref1 = toMake.eventFunc;
+        for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
+          eventName = _ref1[_j];
+          el.addEventListener(eventName, toMake.eventFunc[eventName]);
+        }
+      }
+      return el;
+    },
+    iterateContent: function(parentEl, hasContents) {
+      var contents, currentEl, i, ia, isTypeOfText, originalEntity, results, _i, _j, _len, _len1, _ref, _ref1;
+      if (hasContents) {
+        results = [];
+        _ref = parentEl.contentEl;
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          i = _ref[_i];
+          originalEntity = parentEl.contentEl[i];
+          currentEl = this.DOM.makeText(originalEntity.elType);
+          isTypeOfText = (_ref1 = originalEntity.elType, __indexOf.call(this.DOM.textNodesArr, _ref1) >= 0) ? true : false;
+          hasContents = typeof originalEntity !== 'undefined' ? true : false;
+          this.DOM.giveAttributes(currentEl, originalEntity, isTypeOfText);
+          contents = this.DOM.iterateContent(originalEntity, hasContents);
+          if (contents !== false && content.length > 0) {
+            for (_j = 0, _len1 = contents.length; _j < _len1; _j++) {
+              ia = contents[_j];
+              currentEl.appendChild(contents[ia]);
+            }
+          }
+          results.push(currentEl);
+        }
+        return results;
+      } else {
+        return false;
+      }
+    },
+    make: function(target, key) {
+      var targets;
+      targets = this.DOM.getElem(target);
+      return [].map.call(targets, function(element) {
+        var el, hasContents, i, isTypeOfText, tomake, _i, _len, _ref;
+        tomake = this.DOM.registeredDOMarray[key];
+        el = this.DOM.makeElem(toMake, elType);
+        isTypeOfText = (_ref = toMake.elType, __indexOf.call(this.DOM.textNodesArr, _ref) >= 0) ? true : false;
+        hasContents = typeof toMake.contentEl !== 'undefined' ? true : false;
+        if (contents !== false && contens.length > 0) {
+          for (_i = 0, _len = contents.length; _i < _len; _i++) {
+            i = contents[_i];
+            el.appendChild(contents[i]);
+          }
+        }
+        return element.appendChild(el);
+      });
+    }
+  };
+
+}).call(this);
---
+++ prespros/coffee/snl-dom.coffee
@@ -0,0 +1,55 @@
+@DOM =
+    textNodesArr: ['a', 'p', 'label', 'span', 'li', 'div', 'button', 'textarea', 'option', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+    registeredDOMarray: []
+    register: (toReg) ->
+        Object.keys(toReg).forEach (key) ->
+            @DOM.registeredDOMarray[key] = toReg[key]
+    getElem: (el) ->
+        document.querySelectorAll el
+    makeElem: (el) ->
+        document.createElement el
+    delElem: (el) ->
+        element = @DOM.getElem el
+        [].map.call element, (el) ->
+            el.parentNode.removeChild el
+    makeText: (el) ->
+        document.createTextNode el
+    giveAttributes: (el, toMake, isTypeOfText) ->
+        if typeof toMake.attributeList isnt 'undefined'
+          for attr in toMake.attributeList
+              el.setAttribute attr, toMake.attributeList[attr]
+        if isTypeOfText is true and typeof toMake.textContent isnt 'undefined'
+            textContent = @DOM.makeText toMake, textContent
+            el.appendChild textContent
+        if typeof tomake.eventFunc isnt 'undefined'
+            for eventName in toMake.eventFunc
+                el.addEventListener eventName, toMake.eventFunc[eventName]
+        el
+    iterateContent: (parentEl, hasContents) ->
+        if hasContents
+            results = []
+            for i in parentEl.contentEl
+                originalEntity = parentEl.contentEl[i]
+                currentEl = @DOM.makeText originalEntity.elType
+                isTypeOfText = if originalEntity.elType in @DOM.textNodesArr then true else false
+                hasContents = if typeof originalEntity isnt 'undefined' then true else false
+                @DOM.giveAttributes currentEl, originalEntity, isTypeOfText
+                contents = @DOM.iterateContent originalEntity, hasContents
+                if contents isnt false and content.length > 0
+                    for ia in contents
+                        currentEl.appendChild contents[ia]
+                results.push currentEl
+            results
+        else
+            false
+    make: (target, key) ->
+        targets = @DOM.getElem target
+        [].map.call targets, (element) ->
+            tomake = @DOM.registeredDOMarray[key]
+            el = @DOM.makeElem toMake, elType
+            isTypeOfText = if toMake.elType in @DOM.textNodesArr then true else false
+            hasContents = if typeof toMake.contentEl isnt 'undefined' then true else false
+            if contents isnt false and contens.length > 0
+                for i in contents
+                    el.appendChild contents[i]
+            element.appendChild el
